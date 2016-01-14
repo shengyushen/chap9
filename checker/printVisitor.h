a865962f08d4b0682f2d577bd33aa1fec9802b8e
shengyushen <shengyushen@icloud.com>
2016-01-14 18:19:16 +0800

    further support of print

--- checker/printVisitor.h
+++ checker/printVisitor.h
@@ -26,6 +26,12 @@ public :
 	template <typename T>
 	bool operator()(std::shared_ptr<T>   ) const { return false; }
 };
+class is_T_statement_NOSPEC : public boost::static_visitor<bool> {
+public :
+	bool operator()(std::shared_ptr<T_statement_NOSPEC>   ) const { return true; }
+	template <typename T>
+	bool operator()(std::shared_ptr<T>   ) const { return false; }
+};
 
 //macros for invoke visitor on T_* types and list of non-T_* types
 #define APP_PRINTV(x)                              \
@@ -575,8 +581,475 @@ public :
 
 	OPERATOR(T_event_declaration,p) { 
 		prt_keyword_space("event");
+		APPLST_PRINTV( mem1 , prt_nothing , prt_comma , prt_nothing);//list_of_event_identifiers
+		prt_keyword_space(";");
 	}
 
+	OPERATOR(T_event_identifier_dimension_list,p) { 
+		APP_PRINTV(mem1) 
+		APPLST_PRINTV( mem2 , prt_nothing , prt_nothing , prt_nothing);//dimension_list
+	}
+	
+	OPERATOR(T_genvar_declaration,p) { 
+		prt_keyword_space("genvar");
+		APPLST_PRINTV( mem1 , prt_nothing , prt_comma , prt_nothing);//identifier
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_task_declaration1,p) {
+		prt_keyword_space("\n task");
+			APP_PRINTV(mem1) 
+			prt_space();
+			APP_PRINTV(mem2) 
+		prt_keyword_space(";\n");
+			APPLST_PRINTV( mem3 , prt_nothing , prt_nothing , prt_nothing);//task_item_declaration_list
+			APP_PRINTV(mem4) 
+		prt_keyword_space("\n endtask\n");
+	}
+	OPERATOR(T_task_declaration2,p) {
+		prt_keyword_space("\n task");
+			APP_PRINTV(mem1) 
+			prt_space();
+			APP_PRINTV(mem2) 
+			prt_keyword_space("(");
+			APPLST_PRINTV( mem3 , prt_nothing , prt_comma, prt_nothing);//task_port_list
+			prt_keyword_space(");\n");
+			APPLST_PRINTV( mem4 , prt_nothing , prt_nothing, prt_nothing);//task_item_declaration_list
+			APP_PRINTV(mem5) 
+			prt_keyword_space("\n endtask\n");
+	}
+	OPERATOR(T_automatic_false,p) {  }
+	OPERATOR(T_automatic_true,p) { prt_keyword_space("automatic"); }
+
+	OPERATOR(T_task_item_declaration_block,p) { APP_PRINTV(mem1) }
+	OPERATOR(T_task_item_declaration_input,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+		prt_keyword_space(";\n");
+	}
+	OPERATOR(T_task_item_declaration_output,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+		prt_keyword_space(";\n");
+	}
+	OPERATOR(T_task_item_declaration_inout,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+		prt_keyword_space(";\n");
+	}
+	OPERATOR(T_tf_input_declaration_reg,p) { 
+		prt_keyword_space("input");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem3) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem4 , prt_nothing , prt_comma , prt_nothing);//identifier
+	}
+	OPERATOR(T_tf_input_declaration_type,p) { 
+		prt_keyword_space("input");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing);//identifier
+	}
+	OPERATOR(T_tf_output_declaration_reg,p) { 
+		prt_keyword_space("output");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem3) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem4 , prt_nothing , prt_comma , prt_nothing);//identifier
+	}
+	OPERATOR(T_tf_output_declaration_type,p) {  
+		prt_keyword_space("output");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing);//identifier
+	}
+	OPERATOR(T_tf_inout_declaration_reg,p) {  
+		prt_keyword_space("inout");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem3) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem4 , prt_nothing , prt_comma , prt_nothing);//identifier
+	}
+	OPERATOR(T_tf_inout_declaration_type,p) {  
+		prt_keyword_space("inout");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing);//identifier
+	}
+	OPERATOR(T_task_port_type_integer,p)  { prt_keyword_space("integer"); }
+	OPERATOR(T_task_port_type_real,p)     { prt_keyword_space("real"); }
+	OPERATOR(T_task_port_type_realtime,p) { prt_keyword_space("realtime"); }
+	OPERATOR(T_task_port_type_time,p)     { prt_keyword_space("time"); }
+
+	OPERATOR(T_task_port_item_input,p)     {
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_tf_io_declaration_gen1,p) {  
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem3) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem4) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem5) 
+	}
+	OPERATOR(T_tf_io_declaration_gen2,p) {  
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem3) 
+	}
+	OPERATOR(T_block_variable_type,p) {  
+		APP_PRINTV(mem1) 
+		APPLST_PRINTV( mem2 , prt_nothing, prt_nothing, prt_nothing);//dimension_list
+	}
+	OPERATOR(T_block_real_type,p) {  
+		APP_PRINTV(mem1) 
+		APPLST_PRINTV( mem2 , prt_nothing, prt_nothing, prt_nothing);//dimension_list
+	}
+	OPERATOR(T_block_item_declaration_reg,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		prt_keyword_space("\n reg");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem3) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem4 , prt_nothing , prt_comma , prt_nothing );// block_variable_type
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_block_item_declaration_integer,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		prt_keyword_space("\n integer");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing );// block_variable_type
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_block_item_declaration_time,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		prt_keyword_space("\n time");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing );// block_variable_type
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_block_item_declaration_real,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		prt_keyword_space("\n real");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing );// block_variable_type
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_block_item_declaration_realtime,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		prt_keyword_space("\n realtime");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing );// block_variable_type
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_block_item_declaration_event,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_block_item_declaration_local_param,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_block_item_declaration_param,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_event_expression_exp,p) { APP_PRINTV(mem1) }
+	OPERATOR(T_event_expression_pos,p) { 
+		prt_keyword_space("posedge");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_event_expression_neg,p) {  
+		prt_keyword_space("negedge");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_event_expression_or,p) { assert(false); }
+
+	OPERATOR(T_case_item,p) {  
+		APPLST_PRINTV( mem1 , prt_nothing , prt_comma , prt_nothing);//expression
+		prt_keyword_space(":");
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_case_item_default,p) {  }
+
+	OPERATOR(T_statement_or_block_item_statement,p) { APP_PRINTV(mem1) }
+	OPERATOR(T_statement_or_block_item_block,p) {  APP_PRINTV(mem1) }
+
+	OPERATOR(T_local_parameter_declaration_1,p) {  
+		prt_keyword_space("localparam");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem3 , prt_nothing , prt_comma , prt_nothing);//param_assignment
+	}
+	OPERATOR(T_local_parameter_declaration_2,p) {  
+		prt_keyword_space("localparam");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_comma , prt_nothing);//param_assignment
+	}
+	OPERATOR(T_reg_false,p) {  }
+	OPERATOR(T_reg_true,p) { prt_keyword_space("reg"); }
+
+	OPERATOR(T_statement_NOSPEC,p) { }
+	OPERATOR(T_statement_blocking_assignment,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+		prt_keyword_space(";\n");
+	}
+	OPERATOR(T_blocking_assignment,p) {  
+		APP_PRINTV(mem1) 
+		prt_keyword_space("=");
+		APP_PRINTV(mem2) 
+		APP_PRINTV(mem3) 
+	}
+	OPERATOR(T_net_lvalue_id,p) { APP_PRINTV(mem1)}
+	OPERATOR(T_net_lvalue_idexp,p) { assert(false); }
+	OPERATOR(T_net_lvalue_lvlist,p) { 
+		APPLST_PRINTV( mem1 , prt_lbrace , prt_comma , prt_rbrace);//net_lvalue
+	}
+	OPERATOR(T_delay_or_event_control_NOSPEC,p) {  }
+	OPERATOR(T_delay_or_event_control_delay_control,p) { APP_PRINTV(mem1) }
+	OPERATOR(T_delay_or_event_control_event_control,p) { APP_PRINTV(mem1) }
+	OPERATOR(T_delay_or_event_control_3,p) { 
+		prt_keyword_space("repeat");
+		prt_keyword_space("(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(")");
+			APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_delay_control_delay_value,p) {
+		prt_keyword_space("#");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_delay_control_mintypmax_expression,p) {  
+		prt_keyword_space("#(");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(")");
+	}
+	OPERATOR(T_event_control_eventid,p) {  
+		prt_keyword_space("@");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_event_control_event_exp,p) {  
+		prt_keyword_space("@");
+		APPLST_PRINTV( mem1 , prt_lparent , prt_comma , prt_rparent);//event_expression_orcomma_list
+	}
+	OPERATOR(T_event_control_start,p) { prt_keyword_space("(*)"); }
+	
+	OPERATOR(T_disable_statement,p) { 
+		prt_keyword_space("\n disable");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(";\n");
+	}
+	OPERATOR(T_event_trigger,p) { 
+		prt_keyword_space("\n ->");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(";\n");
+	}
+	OPERATOR(T_loop_statement_forever,p) {  
+		prt_keyword_space("forever\n");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_loop_statement_repeat,p) {  
+		prt_keyword_space("repeat(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(")\n");
+			APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_loop_statement_while,p) {  
+		prt_keyword_space("while(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(")\n");
+			APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_loop_statement_for,p) {  
+		prt_keyword_space("while(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(":");
+			APP_PRINTV(mem2) 
+		prt_keyword_space(":");
+			APP_PRINTV(mem3) 
+		prt_keyword_space(")\n");
+			APP_PRINTV(mem4) 
+	}
+	OPERATOR(T_net_assignment,p) {  
+		APP_PRINTV(mem1) 
+		prt_keyword_space("=");
+		APP_PRINTV(mem2) 
+	}
+
+	OPERATOR(T_statement_case_statement,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_conditional_statement,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_disable_statement,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_event_trigger,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_loop_statement,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_nonblocking_assignment,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_statement_par_block,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_procedural_continuous_assignments,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_statement_procedural_timing_control_statement,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_seq_block,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_system_task_enable,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_task_enable,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_statement_wait_statement,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2) 
+	}
+	
+	OPERATOR(T_conditional_statement_ifelse,p) {  
+		prt_keyword_space("if(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(")");
+			APP_PRINTV(mem2) 
+		if(false==(boost::apply_visitor(is_T_statement_NOSPEC(),*(p->mem3)))) {
+			prt_keyword_space("else");
+			APP_PRINTV(mem3) 
+		}
+	}
+	OPERATOR(T_conditional_statement_ifelseif,p) { assert(false); }
+	OPERATOR(T_case_statement_case,p) { 
+		prt_keyword_space("case(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(")");
+			APPLST_PRINTV( mem2 , prt_nothing , prt_nothing , prt_nothing);//case_item_list
+		prt_keyword_space("\n endcase\n");
+	}
+	OPERATOR(T_case_statement_casez,p) {  
+		prt_keyword_space("casez(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(")");
+			APPLST_PRINTV( mem2 , prt_nothing , prt_nothing , prt_nothing);//case_item_list
+		prt_keyword_space("\n endcase\n");
+	}
+	OPERATOR(T_case_statement_casex,p) {  
+		prt_keyword_space("casex(");
+			APP_PRINTV(mem1) 
+		prt_keyword_space(")");
+			APPLST_PRINTV( mem2 , prt_nothing , prt_nothing , prt_nothing);//case_item_list
+		prt_keyword_space("\n endcase\n");
+	}
+
+	OPERATOR(T_nonblocking_assignment,p) {  
+		APP_PRINTV(mem1) 
+		prt_keyword_space("<=");
+		APP_PRINTV(mem2) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem3) 
+	}
+	OPERATOR(T_par_block,p) {
+		prt_keyword_space("fork");
+		APP_PRINTV(mem1) 
+		prt_keyword_space("\n");
+		APPLST_PRINTV( mem2 , prt_nothing , prt_nothing , prt_nothing);//statement_or_block_item_list
+		prt_keyword_space("\n end\n");
+	}
+
+	OPERATOR(T_colon_block_identifier_NOSPEC,p) {  }
+	OPERATOR(T_colon_block_identifier,p) {  
+		prt_keyword_space(":");
+		APP_PRINTV(mem1) 
+	}
+
+	OPERATOR(T_procedural_continuous_assignments_assign,p) {  
+		prt_keyword_space("assign");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_procedural_continuous_assignments_deassign,p) {
+		prt_keyword_space("deassign");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_procedural_continuous_assignments_force1,p) {  
+		prt_keyword_space("force");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_procedural_continuous_assignments_force2,p) { assert(false);  }
+	OPERATOR(T_procedural_continuous_assignments_release1,p) {  
+		prt_keyword_space("release");
+		APP_PRINTV(mem1) 
+	}
+	OPERATOR(T_procedural_continuous_assignments_release2,p) { assert(false); }
+	OPERATOR(T_procedural_timing_control_statement,p) {  
+		APP_PRINTV(mem1) 
+		prt_keyword_space(" ");
+		APP_PRINTV(mem2) 
+	}
+	OPERATOR(T_procedural_timing_control_delay,p) { APP_PRINTV(mem1) }
+	OPERATOR(T_procedural_timing_control_event,p) { APP_PRINTV(mem1) }
+
+	OPERATOR(T_seq_block,p) { 
+		prt_keyword_space("begin");
+		APP_PRINTV(mem1) 
+		APPLST_PRINTV( mem2 , prt_nothing , prt_nothing , prt_nothing);//statement_or_block_item_list
+		prt_keyword_space("end");
+	}
+	
+	OPERATOR(T_system_task_enable,p) { 
+		APP_PRINTV(mem1)
+		APPLST_PRINTV(mem2 , prt_lparent , prt_comma , prt_lparent)  
+		prt_keyword_space(";");
+	}
+
+	OPERATOR(T_task_enable,p) { 
+		APP_PRINTV(mem1) 
+		APPLST_PRINTV(mem2 , prt_lparent , prt_comma , prt_lparent)  
+		prt_keyword_space(";");
+	}
+	OPERATOR(T_wait_statement,p) { 
+		prt_keyword_space("wait (");
+		APP_PRINTV(mem1) 
+		prt_keyword_space(")\n");
+		APP_PRINTV(mem2) 
+	}
 	OPERATOR(T_module_item__specify_block,p) { assert(false); }
 	OPERATOR(T_module_item__parameter_declaration,p) { 
 		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
@@ -612,9 +1085,15 @@ public :
 		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
 		APP_PRINTV(mem2);
 	}
+	OPERATOR(T_module_item__genvar_declaration,p) { 
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2);
+	}
+	OPERATOR(T_module_item__task_declaration,p) {  
+		APPLST_PRINTV( mem1 , prt_lp_star , prt_comma , prt_star_rp);//attribute_instance_list
+		APP_PRINTV(mem2);
+	}
 // above is finished
-	OPERATOR(T_module_item__genvar_declaration,p) {  }
-	OPERATOR(T_module_item__task_declaration,p) {  }
 	OPERATOR(T_module_item__function_declaration,p) {  }
 	OPERATOR(T_module_item__local_parameter_declaration,p) {  }
 	OPERATOR(T_module_item__parameter_override,p) {  }
