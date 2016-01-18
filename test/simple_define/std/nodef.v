513cf7dd09a2def003360db496219921cdf81ff5
shengyushen <shengyushen@icloud.com>
2016-01-18 16:49:35 +0800

    handling multi-line macros

--- test/simple_define/std/nodef.v
+++ test/simple_define/std/nodef.v
@@ -1,11 +1,21 @@
+`line 1 src/define.v 1
+`line 3 src/define.v 1
 
+`line 5 src/define.v 1
+`line 6 src/define.v 1
+`line 9 src/define.v 1
+`line 12 src/define.v 1
 module i1();
 
 input a;
 output b;
 output c;
-assign b = (1+(2+1)*3* 4)+(1+(2+1)+3* 4)+3* 4-1+(2+1) + 1+(2+1)+3* 4 ;
-assign c = 7-5+1;
+assign b = (1+(2+1)*3* 4)+(1+(2+1)+3* 4) +3* 4-1+(2+1) + 1+(2+1) +3* 4 ;
+assign c = 7-  
+  5 + 
+	
+12 +
+21 +1;
 
 endmodule
 
--- test/simple_define/std/noinclude.v
+++ test/simple_define/std/noinclude.v
@@ -1,8 +1,14 @@
+`line 1 src/define.v 1
 `define \x (x,y) `\y (x,y) + `\z (x,y)
 
 `define \y (x,y) (x*y)+`\z ((x+y),y-x)
-`define \z (a,b) a+b
-`define \w (a,b) a-b
+`define \z (a,b) a +b
+`define \w (a,b) a-  \
+  b + \
+	`\v   
+`define \v \
+12 +\
+21
 module i1();
 
 input a;
