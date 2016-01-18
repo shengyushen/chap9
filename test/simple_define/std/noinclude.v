3b289fbef5338a209c67e5d5aca36277b15a9ac1
shengyushen <shengyushen@icloud.com>
2016-01-18 15:13:56 +0800

    supporting error number adding simple_define testbench to test nested ',' and ()

---
+++ test/simple_define/std/noinclude.v
@@ -0,0 +1,16 @@
+`define \x (x,y) `\y (x,y) + `\z (x,y)
+
+`define \y (x,y) (x*y)+`\z ((x+y),y-x)
+`define \z (a,b) a+b
+`define \w (a,b) a-b
+module i1();
+
+input a;
+output b;
+output c;
+assign b = `\x (1+(2+1) ,3* 4 ) ;
+assign c = `\z (`\w (7,5) , 1 );
+
+endmodule
+
+
---
+++ test/simple_define/std/sdf.v
@@ -0,0 +1,18 @@
+
+ module  i1  
+  
+ (
+      );
+ input     a  ;
+ 
+ output     b  ;
+ 
+ output     c  ;
+ 
+ assign        b  =  ( 1 +  ( 2 + 1 )  * 3 * 4 )  +  ( 1 +  ( 2 + 1 )  + 3 * 4 )  + 3 * 4 - 1 +  ( 2 + 1 )  + 1 +  ( 2 + 1 )  + 3 * 4 ;
+ 
+ assign        c  = 7 - 5 + 1 ;
+ 
+ 
+ endmodule
+ 
